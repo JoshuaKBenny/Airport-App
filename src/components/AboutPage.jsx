478f1dbf6b89f5496d5eb96a368d50c39479a906
JoshuaKBenny <joshuakbenny@gmail.com>
2024-06-23 21:55:20 +0530

    airport app

---
+++ src/components/AboutPage.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+      
+
+
+<div className="container">
+    
+
+
+<div class="card">
+  <div class="card-body">
+    
+
+
+
+  <div className="row g-3">
+<div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+<center>
+
+<h1>About the App</h1>
+Our app was created to revolutionize the airport experience, making travel easier and more enjoyable.
+<br />
+<h1>Creators</h1>
+Developed by a team of travel enthusiasts and tech experts who understand the nuances of air travel.
+<br />
+<h1>Motivation</h1>
+Born out of frustration with outdated airport information systems and a desire to streamline the travel process.
+<br />
+<h1>History</h1>
+Launched in 2017, our app has continuously evolved with new features and improvements based on user feedback.
+<br />
+<h1>Vision and Mission</h1>
+Our mission is to provide a seamless and stress-free airport experience for travelers worldwide.
+</center>
+
+
+
+
+    
+</div>
+</div>
+
+
+
+
+
+  </div>
+</div>
+
+
+</div>
+
+
+    </div>
+  )
+}
+
+export default AboutPage
+
+
+
+
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <div className="container">
+
+
+      <div className="row g-3">
+            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-12">
+               <center> <h1>WELCOME TO FLYX YOUR ULTIMATE COMPANION FOR ALL YOUR AIRPORT NEEDS</h1> </center>
+            </div>
+        </div>      
+
+
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xk-12 col-xxl-12">
+
+
+
+            <div id="carouselExampleFade" class="carousel slide carousel-fade">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://t4.ftcdn.net/jpg/02/10/94/23/360_F_210942390_FVQw9DEE67GupDvM4DDQ8dVDc9XSg9Xm.jpg" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="https://static1.pocketlintimages.com/wordpress/wp-content/uploads/wm/2024/04/iphone-travel-hack-flight-preview.jpg" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.futuretravelexperience.com/wp-content/uploads/2024/05/Fly-Delta-app.jpg" class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+
+            </div>
+        </div>
+        <div className="row g-3">
+            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+
+            <div class="card">
+  <img src="https://cdn.7labs.io/wp-content/uploads/2012/09/Get-real-time-flight-information-using-Live-flight-tracker.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+   <center> <p class="card-text">Airport Navigation and Maps.</p></center>
+  </div>
+</div>
+
+            </div>
+            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+
+
+            <div class="card">
+  <img src="https://img.freepik.com/free-vector/time-travel-cartoon-landing-page-passenger-airplane-flying-sky-buy-ticket-online-concept-with-plane-flight-booking-service-airline-traveling-journey-vacation-holidays-vector-web-banner_107791-9220.jpg?w=360" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <center><p class="card-text">Booking Services</p></center>
+  </div>
+</div>
+
+
+
+            </div>
+            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+
+
+            <div class="card">
+  <img src="https://media.istockphoto.com/id/1289638785/vector/lowest-fare-vector-poster-with-realistic-airplane.jpg?s=612x612&w=0&k=20&c=To6R-dUdKR-qzIwuAL5efURsi71QGwiUyxQLtH5aRWQ=" class="card-img-top" alt="..."/>
+  <div class="card-body">
+  <center>  <p class="card-text">Information on airport amenities.</p></center>
+  </div>
+</div>
+
+
+
+            </div>
+        </div>
+
+<br />
+        <div className="row g-3">
+            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-12">
+               <center> <p>Our app is designed to enhance your airport experience by providing real-time updates, essential information, and various services at your fingertips</p></center>
+            </div>
+        </div>      
+
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
